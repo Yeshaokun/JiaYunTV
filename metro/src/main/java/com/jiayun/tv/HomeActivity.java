b3be304371c72fb9ec30cab07c204103b7436c62
Administrator <631442030@qq.com>
2020-03-22 14:47:38 +0800

    Initial commit

---
+++ metro/src/main/java/com/jiayun/tv/HomeActivity.java
@@ -0,0 +1,16 @@
+
+package com.jiayun.tv;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class HomeActivity extends Activity {
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
